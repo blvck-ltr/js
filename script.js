e56aa160e608609d550e2745c20911aad12b4702
blvck-ltr <120496815+blvck-ltr@users.noreply.github.com>
2025-04-08 22:13:17 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('https://webhook.site/a371d5e0-5163-442d-be83-64b85edfd660')
+    .then(response => response.text())
+    .then(data => {
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
