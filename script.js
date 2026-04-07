b7911f52c4514698d05c7bdffdfdf701f1aec96a
blvck-ltr <120496815+blvck-ltr@users.noreply.github.com>
2026-04-07 09:55:04 +0700

    Remove XSS related code from script.js
    Removed fetch call and console log for XSS.

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-
-    console.log('XSS from Iframe')
-    fetch('https://webhook.site/aedf2ca2-5c80-47a3-bc39-a233c72701cb')
-        .then(response => response.text())
-        .then(data => {
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
+alert('XSS')
 
 
 
